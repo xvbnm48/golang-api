3bb256d3a07f87fa20f6aa6285f2b894390d1ef1
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-09-09 22:03:30 +0700

    go testing

---
+++ go-testing/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
+
+func main() {
+	fmt.Println("GO TESTING TUTORIAL")
+
+	result := Calculate(2)
+	fmt.Println(result)
+}
